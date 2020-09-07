468dc028d5a26cb47d135cac47807a20da88b8bc
turbo-hacker2004 <safin.nadir2001@gmail.com>
2020-09-07 13:04:59 +0300

    cleaning

--- third.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function (){
- $('.table td:nth-child(2)').each(function(){
-   var text = $(this).html();
-
-   $(this).html('<a href="https://www.google.ru">'+text+'</a>');
- })
-
-
-})
